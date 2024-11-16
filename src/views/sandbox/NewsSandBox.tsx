c2decfbfaa3f1c1b18661b96078595315dea6235
xubangjun <bangjun.xu@ucdconnect.ie>
2024-11-16 19:51:07 +0000

    change logo name

--- src/views/sandbox/NewsSandBox.tsx
+++ src/views/sandbox/NewsSandBox.tsx
@@ -56,7 +56,7 @@ function NewsSandBox() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            BANGJUN
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
